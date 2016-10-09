86b023bafe761bc99fac58f7f502499ac690578d
kriesvr <kries.vr@gmail.com>
2016-10-09 16:47:48 +0200

    the data set has been changed to include factors

--- data.R
+++ data.R
@@ -1,10 +1,12 @@
 practicedata <-
-structure(list(Participant = 1:20, Gender = c(1L, 2L, 1L, 1L, 
-1L, 2L, 2L, 1L, 2L, 1L, 1L, 2L, 2L, 2L, 2L, 2L, 1L, 1L, 1L, 1L
-), Race = c(1L, 3L, 2L, 4L, 2L, 4L, 4L, 1L, 2L, 4L, 4L, 4L, 1L, 
-2L, 4L, 3L, 3L, 1L, 4L, 2L), Age = c(12L, 13L, 14L, 12L, 12L, 
-13L, 14L, 14L, 13L, 15L, 16L, 15L, 15L, 14L, 16L, 10L, 11L, 12L, 
-12L, 11L)), .Names = c("Participant", "Gender", "Race", "Age"
-), class = "data.frame", row.names = c(NA, -20L))
+structure(list(Participant = 1:20, Gender = structure(c(1L, 2L, 
+1L, 1L, 1L, 2L, 2L, 1L, 2L, 1L, 1L, 2L, 2L, 2L, 2L, 2L, 1L, 1L, 
+1L, 1L), .Label = c("male", "female"), class = "factor"), Race = structure(c(1L, 
+3L, 2L, 4L, 2L, 4L, 4L, 1L, 2L, 4L, 4L, 4L, 1L, 2L, 4L, 3L, 3L, 
+1L, 4L, 2L), .Label = c("African", "Asian", "Caucasian", "Hispanic"
+), class = "factor"), Age = c(12L, 13L, 14L, 12L, 12L, 13L, 14L, 
+14L, 13L, 15L, 16L, 15L, 15L, 14L, 16L, 10L, 11L, 12L, 12L, 11L
+)), .Names = c("Participant", "Gender", "Race", "Age"), row.names = c(NA, 
+-20L), class = "data.frame")
 testscore <-
 20:39
