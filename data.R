449edeaa6fbb84a15f4503ac2fdfb65bd4e195cf
kriesvr <kries.vr@gmail.com>
2016-10-09 17:04:33 +0200

    a new variable has been added to the data set

--- data.R
+++ data.R
@@ -6,7 +6,8 @@ structure(list(Participant = 1:20, Gender = structure(c(1L, 2L,
 1L, 4L, 2L), .Label = c("African", "Asian", "Caucasian", "Hispanic"
 ), class = "factor"), Age = c(12L, 13L, 14L, 12L, 12L, 13L, 14L, 
 14L, 13L, 15L, 16L, 15L, 15L, 14L, 16L, 10L, 11L, 12L, 12L, 11L
-)), .Names = c("Participant", "Gender", "Race", "Age"), row.names = c(NA, 
--20L), class = "data.frame")
+), Test = c(50, 66, 75, 78, 44, 57, 78, 56, 87, 56, 55, 90, 68, 
+76, 67, 45, 76, 67, 89, 50)), .Names = c("Participant", "Gender", 
+"Race", "Age", "Test"), row.names = c(NA, -20L), class = "data.frame")
 testscore <-
 20:39
