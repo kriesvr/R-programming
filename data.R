442d488e82b4ddbdcbc5ddcb838589a4021a11ee
kriesvr <kries.vr@gmail.com>
2016-10-09 16:28:06 +0200

    An R data dump have been included

---
+++ data.R
@@ -0,0 +1,10 @@
+practicedata <-
+structure(list(Participant = 1:20, Gender = c(1L, 2L, 1L, 1L, 
+1L, 2L, 2L, 1L, 2L, 1L, 1L, 2L, 2L, 2L, 2L, 2L, 1L, 1L, 1L, 1L
+), Race = c(1L, 3L, 2L, 4L, 2L, 4L, 4L, 1L, 2L, 4L, 4L, 4L, 1L, 
+2L, 4L, 3L, 3L, 1L, 4L, 2L), Age = c(12L, 13L, 14L, 12L, 12L, 
+13L, 14L, 14L, 13L, 15L, 16L, 15L, 15L, 14L, 16L, 10L, 11L, 12L, 
+12L, 11L)), .Names = c("Participant", "Gender", "Race", "Age"
+), class = "data.frame", row.names = c(NA, -20L))
+testscore <-
+20:39
